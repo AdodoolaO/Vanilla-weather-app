ea57cf9032cfad5ad60a712f8c32ba3eca61200e
AdodoolaO <adodoola.omiyale@outlook.com>
2021-11-18 18:00:28 +0000

    Added API integrations

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#cityInput").value;
+  let apiKey = "04ceffe59fa10e5e23c4c7bf0b0e4eca";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
 
+let city = document.querySelector("#form");
+city.addEventListener("submit", searchCity);
+
+function showWeather(response) {
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  let temperature = document.querySelector("#temperature");
+  let showTemperature = response.data.main.temp;
+  temperature.innerHTML = Math.round(showTemperature);
+  let wind = document.querySelector("#speed");
+  let showWind = response.data.wind.speed;
+  wind.innerHTML = Math.round(showWind);
+  let humidity = document.querySelector("#humid");
+  let showHumidy = response.data.main.humidity;
+  humidity.innerHTML = showHumidy;
+  let descrip = document.querySelector("#description");
+  let showDescrip = response.data.weather[0].description;
+  descrip.innerHTML = showDescrip;
+}
