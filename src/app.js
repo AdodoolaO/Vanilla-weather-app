3a86e9e5fa20c383580e365597b551c618a26f89
AdodoolaO <adodoola.omiyale@outlook.com>
2021-11-20 14:41:26 +0000

    Add Unitconvert functions to  JS

--- src/app.js
+++ src/app.js
@@ -6,9 +6,6 @@ function searchCity(event) {
   axios.get(apiUrl).then(showWeather);
 }
 
-let city = document.querySelector("#form");
-city.addEventListener("submit", searchCity);
-
 function showWeather(response) {
   document.querySelector("#cityName").innerHTML = response.data.name;
   let temperature = document.querySelector("#temperature");
@@ -23,4 +20,38 @@ function showWeather(response) {
   let descrip = document.querySelector("#description");
   let showDescrip = response.data.weather[0].description;
   descrip.innerHTML = showDescrip;
+  celsius.classList.add("active");
+  farenHeit.classList.remove("active");
 }
+
+function convertBackToCelsius(event) {
+  event.preventDefault();
+  let city = document.querySelector("#cityInput").value;
+  let apiKey = "04ceffe59fa10e5e23c4c7bf0b0e4eca";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function convertUnitToFarenheit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#cityInput").value;
+  let apiKey = "04ceffe59fa10e5e23c4c7bf0b0e4eca";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showConvert);
+}
+
+function showConvert(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp * 1.8 + 32);
+  celsius.classList.remove("active");
+  farenHeit.classList.add("active");
+}
+
+let farenHeit = document.querySelector("#farenheit");
+farenHeit.addEventListener("click", convertUnitToFarenheit);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", convertBackToCelsius);
+
+let city = document.querySelector("#form");
+city.addEventListener("submit", searchCity);
