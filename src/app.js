acf11efd0551a23cf0f8ae2e06ba74fd298b887f
AdodoolaO <adodoola.omiyale@outlook.com>
2021-12-22 18:08:27 +0000

    Updated JS

--- src/app.js
+++ src/app.js
@@ -56,6 +56,8 @@ function displayForcast(response) {
   let forcastElement = document.querySelector("#forcast");
   //let days = ["Weds", "Thurs", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
+  //looping to display/inject multiple forcasts
+
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
@@ -79,13 +81,16 @@ function displayForcast(response) {
   console.log(forecastHTML);
 }
 
-//looping to display/inject multiple forcasts
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#cityInput").value;
+function search(city) {
   let apiKey = "04ceffe59fa10e5e23c4c7bf0b0e4eca";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
+}
+
+function submitHandle(event) {
+  event.preventDefault();
+  let city = document.querySelector("#cityInput");
+  search(city.value);
 }
 function getForecast(coordinates) {
   console.log(coordinates);
@@ -121,7 +126,6 @@ function showWeather(response) {
   celsius.classList.add("active");
   farenHeit.classList.remove("active");
   getForecast(response.data.coord);
-  displayForcast();
 }
 
 function convertBackToCelsius(event) {
@@ -153,7 +157,7 @@ farenHeit.addEventListener("click", convertUnitToFarenheit);
 let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", convertBackToCelsius);
 
-let city = document.querySelector("#form");
-city.addEventListener("submit", searchCity);
+search("London");
 
-searchCity(London);
+let city = document.querySelector("#form");
+city.addEventListener("submit", submitHandle);
